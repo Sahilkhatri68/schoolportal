1f2d12f60b8e2473664756155a881c3f80a6ac9b
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2023-10-21 08:53:33 +0530

    changes

---
+++ models/TeacherSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const teacherschema = new mongoose.Schema({
+  teachername: {
+    type: String,
+    require: true,
+  },
+  teacherage: {
+    type: String,
+    require: true,
+  },
+  teacheremial: {
+    type: String,
+    require: true,
+  },
+  teacherpassword: {
+    type: String,
+    require: true,
+  },
+  teacheraddress: {
+    type: String,
+    require: true,
+  },
+  teacherqualification: {
+    type: String,
+    require: true,
+  },
+  assignedclass: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "classSchema",
+    },
+  ],
+  joindate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("teacherschema", teacherschema);
